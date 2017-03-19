5c0a320c2899504e41066d43db44273e3b861a67
muehan <tdc@gmx.ch>
2017-03-19 21:06:12 +0100

    project start

---
+++ SharedListMobile/SharedListMobile/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharedListMobile.services;
+using Xamarin.Forms;
+
+
+namespace SharedListMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SharedListMobile.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            var firebase = new FirebaseService();
+
+            firebase.Init();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SharedListMobile/SharedListMobile/services/FirebaseService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Firebase.Xamarin.Database;
+using Firebase.Xamarin.Database.Query;
+using System.Diagnostics;
+using System.Linq;
+using Firebase.Xamarin.Auth;
+
+namespace SharedListMobile.services
+{
+    public class FirebaseService
+    {
+        public async void Init()
+        {
+            var firebase = new FirebaseClient("https://sharedlist-40f8d.firebaseio.com");
+
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyBBeAqd9qBd_H_JkU5b3YVvWRIFqewqiE0"));
+
+            var user = await authProvider.SignInWithEmailAndPasswordAsync("yourEmail", "yourPassword");
+
+            Debug.WriteLine($"user: {user.User.Email}");
+
+            IReadOnlyCollection<FirebaseObject<ShoppingItem>> items = await firebase
+              .Child("sharedList/shoppingList")
+              .WithAuth(user.FirebaseToken)
+              .OnceAsync<ShoppingItem>();
+
+            Debug.WriteLine($"item count {items.Count}");
+
+            foreach (var item in items)
+            {
+                Debug.WriteLine($"{item.Key} name is {item.Object.value}");
+            }
+        }
+    }
+
+    public class ShoppingLists
+    {
+        public IEnumerable<ShoppingItem> ShoppingItems { get; set; }
+    }
+
+    public class ShoppingItem
+    {
+        public string key { get; set; }
+
+        public string value { get; set; }
+    }
+}
